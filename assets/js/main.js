4952137be0233ea0fcef6760487c2d5317f385bc
Marco Afonso <marco.afonso@soundparticles.com>
2019-05-24 01:07:33 +0100

    added a bunch of animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,49 @@
 $(document).ready(
 	function(){
+
+		var anim = anime({
+			targets: '.page-frame',
+			opacity: [0, 1],
+			duration: 600,
+			delay: 0,
+			easing: 'easeOutQuad'
+
+
+		});
+
 		$("header").find("ul").addClass("header-menu");
 		$("header").find("li").addClass("header-menu-item");
 		anime({
-			targets: ".header-menu-item, .header-logo",
+			targets: ".header-menu-item, .header-logo, .open-menu-button",
 			translateY: [-20, 0],
 			opacity: [0, 1],
 			duration: 800,
 			delay: anime.stagger(100)
 		});
 		anime({
-			targets: ".page-body",
+			targets: ".page-body-content",
 			translateY: [100, 0],
 			opacity: [0, 1],
 			duration: 600,
 			easing: 'easeOutCubic',
 			delay: anime.stagger(100)
 		});
-	}
-);
+
+		anime({
+			targets: '.triangle-wrapper',
+			scaleY: [0,1],
+			easing: 'easeOutCubic',
+			duration: 500
+		});
+		setTimeout(function(){
+			anime.remove('.page-frame, .header-menu-item, .header-logo, .open-menu-button');
+			$('.page-frame').removeAttr('style');
+			$('.header-logo').removeAttr('style');
+			$('.header-menu-item').removeAttr('style');
+			$('.open-menu-button').removeAttr('style');
+
+		}, 1000);
+	});
 
 function toggleMenu(){
 	var ul = $(".header-menu");
